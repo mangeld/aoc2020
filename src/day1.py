459b53542fc2ced6369fc3114e0a2739eb6c2805
Miguel Ángel Durán González <hi@mangel.me>
2020-12-01 17:48:07 +0100

    Fix types

--- src/day1.py
+++ src/day1.py
@@ -1,20 +1,23 @@
 from sys import argv
-from typing import List
+from typing import List, Tuple
 from itertools import combinations
 from functools import reduce
 
 
-def find_2020_entries(entries: List[int], n_products=2) -> List[int]:
+def find_2020_entries(entries: List[int], n_products=2) -> Tuple[int, ...]:
     for combination in combinations(entries, n_products):
         if sum(combination) == 2020:
             return combination
+    return ()
 
 
 if __name__ == '__main__':
     with open(argv[1]) as quizz_input:
         numbers = list(map(int, quizz_input.readlines()))
+        multiply = lambda int_list: reduce(lambda a, b: a * b, int_list)
+
         answer = find_2020_entries(numbers)
-        print("2020 Entries:", answer)
-        print("First answer:", answer[0] * answer[1])
+        print("First answer:", multiply(answer))
+
         answer = find_2020_entries(numbers, 3)
-        print("Second answer:", reduce(lambda a, b: a * b, answer))
+        print("Second answer:", multiply(answer))
